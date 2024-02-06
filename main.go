82c4aec5a3e6864d90b14207efa6fdad0c482549
sadegh <adyani.sadegh@gmail.com>
2024-02-06 14:06:05 +0330

    basic simulation of boids

--- main.go
+++ main.go
@@ -9,11 +9,14 @@ import (
 const (
 	screenWidth, screenHeight = 640, 360
 	boidCount                 = 500
+	viewRadius                = 13
+	adjRate                   = 0.015
 )
 
 var (
-	green = color.RGBA{R: 10, G: 255, B: 50, A: 255}
-	boids [boidCount]*Boid
+	green   = color.RGBA{R: 10, G: 255, B: 50, A: 255}
+	boids   [boidCount]*Boid
+	boidMap [screenWidth + 1][screenHeight + 1]int
 )
 
 type Game struct {
@@ -37,9 +40,16 @@ func (g *Game) Layout(_, _ int) (w, h int) {
 }
 
 func main() {
+	for i, row := range boidMap {
+		for j := range row {
+			boidMap[i][j] = -1
+		}
+	}
+
 	for i := 0; i < boidCount; i++ {
 		createBoid(i)
 	}
+
 	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
 	ebiten.SetWindowTitle("Boids in a box")
 	if err := ebiten.RunGame(&Game{}); err != nil {
